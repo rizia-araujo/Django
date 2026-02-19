4a6c0be60329ec20c831f11d3ed727c5d121ccff
Rízia Araujo <rizia.araujo12@gmail.com>
2026-02-19 14:21:48 -0300

    Configurações de segurança para produção

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,3 +121,5 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
